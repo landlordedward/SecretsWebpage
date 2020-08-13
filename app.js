f92b8338e84e41389d930e7b1d6b45eaa76dd00a
Eddie <landlordedward@gmail.com>
2020-08-12 22:07:46 -0700

    Change from encrypted username + pw to hash.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 // const _ = require("lodash");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -25,7 +26,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // Add encryption
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -53,9 +54,10 @@ app.get("/login", function(req, res) {
 app.post("/register", function(req, res) {
   const emailInput = req.body.username;
   const passwordInput = req.body.password;
+  const passwordHash = md5(passwordInput);
   const newUser = new User({
     email:emailInput,
-    password:passwordInput
+    password:passwordHash
   });
   newUser.save(function(err) {
     if(!err) {
@@ -67,12 +69,13 @@ app.post("/register", function(req, res) {
 app.post("/login", function(req, res) {
   const emailInput = req.body.username;
   const passwordInput = req.body.password;
+  const passwordHash = md5(passwordInput);
   User.findOne({email:emailInput}, function(err, user) {
     if (err) {
       console.log("Failed");
       res.redirect("/login");
     } else {
-      if (user.password === passwordInput) {
+      if (user.password === passwordHash) {
         res.render("secrets");
       } else {
         console.log("Failed");
